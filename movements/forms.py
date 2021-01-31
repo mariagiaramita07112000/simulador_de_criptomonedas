036a510813c603fb180a6bd141c6be8e38ceb347
mariagiaramita07112000 <guarapitamaria@gmail.com>
2021-01-31 20:37:04 +0100

    validaciones 2

--- movements/forms.py
+++ movements/forms.py
@@ -1,66 +1,73 @@
+from movements import app
 from flask_wtf import FlaskForm
 from wtforms import IntegerField,DateField, TimeField,SelectField, StringField, FloatField, SubmitField
 from wtforms.validators import DataRequired, Length, ValidationError
 import sqlite3
-
-to_currencies=[]
-from_currencies=[]
-
-
-conn= sqlite3.connect("movements/data/basededatos.db")
-c= conn.cursor()
-
-c.execute("SELECT SUM(to_quantity) AS total, to_currency FROM movimientos GROUP BY to_currency;")
-to_currencies= c.fetchall()
-c.execute("SELECT SUM(from_quantity) AS total, from_currency FROM movimientos GROUP BY from_currency")  
-from_currencies= c.fetchall()
-
-result_currencies = []
-for to_currency in to_currencies:
-    is_coincidence = False
-    for from_currency in from_currencies:
-        if to_currency[1] == from_currency[1]:
-            is_coincidence = True
-            if (to_currency[0] - from_currency[0]) > 0:
-                result_currencies.append(to_currency[1])
-    if is_coincidence == False: 
-        result_currencies.append(to_currency[1])
-
-result_currencies.append('EUR')
-
-conn.commit()
-conn.close()
+from flask import render_template
 
 symbol_cripto=["EUR","EOS","XRP", "BCH", "ADA", "ETH","XLM","BTC", "USDT","LTC","BNB","TRX","BSV"]
+messages=[]
 
 def validate_currencies(form, field):
     if form.data['from_currency'] == form.data['to_currency']:
         raise ValidationError('Las monedas deben ser distintas')
 
 def validate_quantity(form,field):
+    try:
+        conn= sqlite3.connect("movements/data/basededatos.db")
+        c= conn.cursor()
+
+    except Exception as e:
+        print(f"no se ha podido conectar con la base de datos{str(e)}")
+        messages.append("error al conectar la api")
+        return render_template("errores.html", messages=messages)
     
-    conn= sqlite3.connect("movements/data/basededatos.db")
-    c= conn.cursor()
+    try:
+        c.execute("SELECT SUM(to_quantity) AS total, to_currency FROM movimientos WHERE to_currency='"+form.data['from_currency']+"' GROUP BY to_currency;")
+        from_currency1=c.fetchone()
+        if from_currency1 is not None:
+            from_currency1= from_currency1[0]
+        else:
+            from_currency1=0
+            print(from_currency1)
+    except Exception as e:
+        print(f"no se ha podido obtener el to_currency de esta moneda{str(e)}")
+        messages.append("error al obtener el to de esta moneda")
+        return render_template("errores.html", messages=messages)
 
-    c.execute("SELECT SUM(to_quantity) AS total, to_currency FROM movimientos WHERE to_currency='"+form.data['from_currency']+"' GROUP BY to_currency;")
-    from_currency1=c.fetchone()[0]
-    print(from_currency1)
-    c.execute("SELECT SUM(from_quantity) AS total, from_currency FROM movimientos WHERE from_currency='"+form.data['from_currency']+"' GROUP BY from_currency;")
-    from_currency2=c.fetchone()[0]
+    try:
+        c.execute("SELECT SUM(from_quantity) AS total, from_currency FROM movimientos WHERE from_currency='"+form.data['from_currency']+"' GROUP BY from_currency;")
+        from_currency2=c.fetchone()
+        if from_currency2 is not None:
+            from_currency2=from_currency2[0]
+        else:
+            from_currency2=0
+    except Exception as e:
+        print(f"no se ha podido obtener el from_currency de la moneda {str(e)}")
+        messages.append("no se ha podido obtener el from de esta moneda ")
+        return render_template("errores.html", messages=messages)
+    
+    try:
+        result=float(from_currency1) - float(from_currency2)
+    except Exception as e: 
+        print(f"hay datos erroneos y no se ha podido realizar la operacion {str(e)}")
+        messages.append("se han introducido datos erroneos y no se ha podido realizar la operacion , por favor revise los datos antes de continuar")
+        return render_template("errores.html", messages=messages)
 
-    result=float(from_currency1) - float(from_currency2)
     print(result)
     print(form.data["from_quantity"])
     if float(form.data['from_quantity']) > result:
         print(form.data['from_quantity'])
         raise ValidationError('no tienes suficiente saldo')
 
+
+
 class MovementForm(FlaskForm):
 
     id= IntegerField("id")
     date=  DateField("date")
     time= TimeField("time")
-    from_currency= SelectField("From" , validators=[DataRequired('seleccciona la moneda'), validate_currencies], choices=result_currencies)
+    from_currency= SelectField("From" , validators=[DataRequired('seleccciona la moneda'), validate_currencies], choices=[])
     from_quantity=  FloatField("Q" , validators=[DataRequired("solo numeros"), validate_quantity])
     to_currency= SelectField("To", validators=[DataRequired('selecciona una opcion'), validate_currencies], choices=symbol_cripto)
     to_quantity= FloatField("Q2")
--- movements/views.py
+++ movements/views.py
@@ -7,59 +7,139 @@ from datetime import datetime , date
 import coinmarketcapapi
 import requests
 
-@app.route("/")
-def listaIngresos():
-    form= MovementForm()
+messages=[]
+
+def get_currencies():
+    to_currencies=[]
+    from_currencies=[]
+
+
     conn= sqlite3.connect("movements/data/basededatos.db")
     c= conn.cursor()
 
-    c.execute("SELECT date, time , from_currency, from_quantity, to_currency, to_quantity, PU FROM movimientos;") or []
-    ingresos= c.fetchall()
+    c.execute("SELECT SUM(to_quantity) AS total, to_currency FROM movimientos GROUP BY to_currency;")
+    to_currencies= c.fetchall()
+    print(to_currencies)
+    c.execute("SELECT SUM(from_quantity) AS total, from_currency FROM movimientos GROUP BY from_currency")  
+    from_currencies= c.fetchall()
 
+    result_currencies = []
+    for to_currency in to_currencies:
+        is_coincidence = False
+        for from_currency in from_currencies:
+            if to_currency[1] == from_currency[1]:
+                is_coincidence = True
+                if (to_currency[0] - from_currency[0]) > 0:
+                    result_currencies.append(to_currency[1])
+        if is_coincidence == False: 
+            result_currencies.append(to_currency[1])
+
+    result_currencies.append('EUR')
+
+    conn.commit()
     conn.close()
+    return result_currencies
+
+def connection():
+    succes=True
+    c=[]
+
+    conn= sqlite3.connect("movements/data/basededatos.db")
+    c=conn.cursor()
+
+    return c
+
+def make_query(query, params=[]):
+    c=[]
+
+    c=connection()
+    c.execute(query, params)
+
+    return c 
+
+@app.route("/")
+def listaIngresos():
+    form= MovementForm()
+    
+    
+    try:
+        result = make_query("SELECT date, time , from_currency, from_quantity, to_currency, to_quantity, PU FROM movimientos;") or []
+        print(result)
+        ingresos= result.fetchall()
+        result.close()
+    except Exception as e:
+        print(f"no ha podido realizar la consulta a la base de datos {type(e).__name__} - {e}")
+        messages.append("lo siento, no se hemos podido conectar con la base de datos")
+        return render_template("errores.html", datos=[] , form=form, messages=messages) 
+
+
+    
 
     '''# llamo a mi fichero csv 
     fIngresos= open("movements/data/basededatos.csv ", "r")
     # leo linea a linea mi fichero 
     csvReader= csv.reader(fIngresos, delimiter="," , quotechar='"')
     ingresos= list(csvReader) '''
-    return render_template("movements.html", datos=ingresos , form=form)  
+    return render_template("movements.html", datos=ingresos , form=form, messages=messages)  
 
 @app.route('/compra.html', methods=['GET','POST'])
 def compraNueva():
     form= MovementForm(csrf_enabled=False)
+    form.from_currency.choices = get_currencies()
     to_quantity = 0
     PU = 0
+    
+    
     print(form.validate_on_submit())
     
     if request.method== 'POST' and form.validate_on_submit():
-
-        today= date.today()
-        today_2= "{}/{}/{}".format(today.day, today.month , today.year)
-        now = datetime.now()
-        time= "{}:{}:{}".format(now.hour, now.minute, now.second)
+        try:
+            today= date.today()
+            today_2= "{}/{}/{}".format(today.day, today.month , today.year)
+            now = datetime.now()
+            time= "{}:{}:{}".format(now.hour, now.minute, now.second)
+        except Exception as e:
+            print(f"no se ha podido obtener la fecha y la hora actual {type(e).__name__} - {e}")
+            messages.append("se ha producido un error al obtener la hora y fecha")
+            return render_template("errores.html", messages=messages)
         
         amount= request.form.get("from_quantity")
         symbol= request.form.get("from_currency")
         convert= request.form.get("to_currency")
 
-        apikey= '8cf2c866-52c9-47d1-a4f9-d06a347da773'
-        url_api="https://pro-api.coinmarketcap.com/v1/tools/price-conversion?amount={}&symbol={}&convert={}&CMC_PRO_API_KEY={}"
-        respuesta= requests.get(url_api.format(amount, symbol , convert, apikey))
-        if respuesta.status_code == 200:
-            datos= respuesta.json()
-            cantidad_2= datos['data'] ['quote'] [convert] ['price']
-            to_quantity = cantidad_2
-            PU = (float(cantidad_2) / float(amount))
-        else:
-            print(respuesta.json()['status'])
+        try:
+            apikey= '8cf2c866-52c9-47d1-a4f9-d06a347da773'
+            url_api="https://pro-api.coinmarketcap.com/v1/tools/price-conversion?amount={}&symbol={}&convert={}&CMC_PRO_API_KEY={}"
+
+            respuesta= requests.get(url_api.format(amount, symbol , convert, apikey))
+            if respuesta.status_code == 200:
+                datos= respuesta.json()
+                cantidad_2= datos['data'] ['quote'] [convert] ['price']
+                to_quantity = cantidad_2
+                PU = (float(cantidad_2) / float(amount))
+            elif respuesta.status_code == 401:
+                print("apikey invalida")
+                messages.append("No se ha podido enviar la solicitud a la api, debido a que ha introducido una apikey invalida")
+                return render_template("errores.html", messages=messages)
+
+            else:
+                print("se ha producido un error  ", respuesta.status_code)
+                messages.append("se ha producido un error en la consulta verifique por favor los datos")
+                return render_template("errores.html", messages=messages)
+        except Exception as e :
+        
+            print(f"no hemos podido conectar con la api{type(e).__name__} - {e} ")
+            messages.append("No se ha podido enviar la solicitud a la api, por favor comuniquese con el administrador")
+            return render_template("errores.html", messages=messages)
+
 
         if request.form.get('submit2') is not None:
             fIngresos= open("movements/data/basededatos.csv" , "w+" , newline="")
             csvWriter = csv.writer(fIngresos, delimiter="," , quotechar='"')
             csvWriter.writerow([request.form.get("Fecha") , request.form.get("Hora"), request.form.get("convert_From"), request.form.get("Q"), request.form.get("convert_To"), request.form.get("Q2"), request.form.get("PU")])
-            conn= sqlite3.connect('movements/data/basededatos.db')
+            conn= sqlite3.connect("movements/data/basededatos.db")
             c= conn.cursor()
+
 
             c.execute('INSERT INTO movimientos (date, time, from_currency, from_quantity, to_currency, to_quantity, "PU") VALUES (?,?,?,?,?,?,?);', 
             (
@@ -72,10 +152,11 @@ def compraNueva():
                 float(request.form.get("PU"))
 
             ))
-    
             conn.commit()
             conn.close()
             return redirect(url_for("listaIngresos"))
+    
+
 
     return render_template("compra.html", form=form, PU=PU, to_quantity=to_quantity)
 
